3c544a68ea331dd7d0dd58dbaf5f6e075b425fab
vasfvitor <gitkey@virtuaires.com.br>
2024-09-16 19:17:03 -0300

    launch window

--- main.py
+++ main.py
@@ -10,6 +10,13 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen.fill('black')
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
