f044ffa836ebe7bd56c544b88fb0edbacc53acf5
vasfvitor <gitkey@virtuaires.com.br>
2024-09-18 15:22:02 -0300

    rotate

--- main.py
+++ main.py
@@ -23,9 +23,12 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-        player.draw(screen)
 
 
 if __name__ == "__main__":
